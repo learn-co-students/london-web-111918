32e839a2560b9fbc372dfc4b1ecedbe1efbe40dc
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2019-01-16 15:46:51 +0000

    edits

---
+++ 26-woof-woof/src/index.js
@@ -0,0 +1,115 @@
+document.addEventListener(`DOMContentLoaded`, init);
+
+function init(e) {
+  const filterDogsButton = document.querySelector('#good-dog-filter');
+  filterDogsButton.addEventListener('click', toggleFilterDogs);
+  getDogs().then(addAllDogsToDogBar);
+}
+
+function toggleFilterDogs() {
+  const filterDogsButton = document.querySelector('#good-dog-filter');
+  if (filterDogsButton.innerText.includes("OFF")) {
+    filterDogsButton.innerText = "Filter good dogs: ON";
+    updateDogBar();
+  } else {
+    filterDogsButton.innerText = "Filter good dogs: OFF";
+    updateDogBar();
+  }
+}
+
+function updateDogBar() {
+  const filterDogsButton = document.querySelector('#good-dog-filter');
+  if (filterDogsButton.innerText.includes("OFF")) {
+    getDogs().then(dogArray => addAllDogsToDogBar(dogArray))
+  } else {
+    getDogs().then(dogArray => addAllDogsToDogBar(dogArray, true))
+  }
+}
+
+const baseURL = `http://localhost:3000/pups`;
+
+function addAllDogsToDogBar(dogArray, filter = false) {
+  const dogBar = document.querySelector(`#dog-bar`);
+  dogBar.innerHTML = "";
+  if (filter) {
+    dogArray.filter(dog => dog.isGoodDog).forEach(addDogSpanToDogBar);
+  } else {
+    dogArray.forEach(addDogSpanToDogBar);
+  }
+}
+
+function addDogSpanToDogBar(dog) {
+  const dogBar = document.querySelector('#dog-bar');
+  const dogSpan = document.createElement('span');
+  dogSpan.innerText = dog.name;
+  dogSpan.dataset.id = dog.id;
+  dogSpan.addEventListener(`click`, onDogSpanClick);
+  dogBar.append(dogSpan);
+}
+
+function onDogSpanClick(e) {
+  getSingleDog(e.target.dataset.id)
+    .then(addDogInfoToPage)
+}
+
+function addDogInfoToPage(dog) {
+  const dogInfo = document.querySelector(`#dog-info`);
+  dogInfo.innerHTML = ``;
+
+  const dogImg = document.createElement(`img`);
+  dogImg.src = dog.image;
+
+  const dogTitle = document.createElement(`h2`);
+  dogTitle.innerText = dog.name;
+
+  const dogButton = document.createElement(`button`);
+  dogButton.innerText = dog.isGoodDog ? "Good Dog!" : "Bad Dog!";
+  dogButton.dataset.id = dog.id;
+  dogButton.addEventListener(`click`, onGoodDogButtonClick);
+
+  dogInfo.append(dogImg, dogTitle, dogButton);
+}
+
+function onGoodDogButtonClick(e) {
+  let newValue;
+  if (e.target.innerText.includes("Good")) {
+    e.target.innerText = "Bad Dog"
+    newValue = false;
+  } else {
+    e.target.innerText = "Good Dog"
+    newValue = true;
+  }
+  toggleGoodDog(e.target.dataset.id, newValue).then(updateDogBar)
+}
+
+/*
+  --------------------------------------
+          The API calls section
+          starts here!
+  --------------------------------------
+*/
+
+function getDogs () {
+  return fetch(baseURL)
+    .then(response => response.json())
+}
+
+function getSingleDog(id) {
+  return fetch(baseURL + `/${id}`)
+    .then(response => response.json())
+}
+
+function toggleGoodDog(id, newValue) {
+  const options = {
+    method: "PATCH",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      isGoodDog: newValue
+    })
+  }
+  return fetch(baseURL + `/${id}`, options)
+    .then(response => response.json())
+}
+

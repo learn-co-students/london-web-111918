559347816dd2d7017712dc0a93740d3acc3c06c7
jonashaefele <hello@jonashaefele.com>
2019-02-14 12:01:20 +0000

    init redux lesson with CRA

---
+++ 39-redux/src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  state = { count: 0 };
+
+  increment = () => {
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header count={this.state.count} />
+        <Counter
+          count={this.state.count}
+          increment={this.increment}
+          decrement={this.decrement}
+        />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  renderDescription = () => {
+    const remainder = this.props.count % 5;
+    const upToNext = 5 - remainder;
+    return `The current count is less than ${this.props.count + upToNext}`;
+  };
+
+  render() {
+    return (
+      <header className="App-header">
+        <h1 className="App-title">The big redux counter</h1>
+        <h3>{this.renderDescription()}</h3>
+      </header>
+    );
+  }
+}
+
+class Counter extends Component {
+  render() {
+    return (
+      <div className="Counter">
+        <h1>{this.props.count}</h1>
+        <button onClick={this.props.decrement}> - </button>
+        <button onClick={this.props.increment}> + </button>
+      </div>
+    );
+  }
+}
+
+export default App;
